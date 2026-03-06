22d0b2b2903f1434ade9d177b9a7b2f7c15231ad
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-03-06 13:53:45 +1300

    Completed solution for the Gift in C#

--- C#/Medium/The Gift.cs
+++ C#/Medium/The Gift.cs
@@ -5,10 +5,6 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Solution
 {
     static void Main(string[] args)
@@ -17,67 +13,57 @@ class Solution
         int giftPrice = int.Parse(Console.ReadLine());
 
         int[] participantBudgets = new int[participantCount];
-        List<int> outputBudgets = new List<int>();
-
-        
-
-        Console.Error.WriteLine("Participant Count: " + participantCount);
-        Console.Error.WriteLine("Gift price: " + giftPrice);
+        List<int> allocation = new List<int>();
 
         for (int i = 0; i < participantCount; i++)
         {
             //Get each participant's budgets
-            participantBudgets.Add(int.Parse(Console.ReadLine()));
+            participantBudgets[i] = int.Parse(Console.ReadLine());
         }
 
-        foreach (int budget in participantBudgets)
-        {
-            Console.Error.WriteLine(budget);
-        }
+        //Sort the available budgets
+        Array.Sort(participantBudgets);
 
-        int[] splitAmount = SplitBudgetAllocation(giftPrice, participantCount);
-
-        int remainder = 0;
-
-        foreach (int participant in participantBudgets)
-        {
-            
-        }
-/*
-        if (remainder > 0)
+        if (participantBudgets.Sum() < giftPrice)
         {
             Console.WriteLine("IMPOSSIBLE");
+            return;
         }
-        else
+        
+        
+        //Start the allocation
+        int remainingSpend = giftPrice;
+        
+        for (int i = 0; i < participantCount; i++)
         {
-            foreach(int amount in outputBudgets)
-            {
-                Console.WriteLine(amount);
-            }
-        }
-*/
-        // Write an answer using Console.WriteLine()
-        // To debug: Console.Error.WriteLine("Debug messages...");
+            //Added these allocation to make it easier to read
+            int budget = participantBudgets[i];
+            int splitAllocation = SplitBudgetAllocation(remainingSpend, participantCount - i);
 
+            if (budget < splitAllocation)
+            {
+                //Person does not have enough
+                //Give entire budget, add leftover to remainder
+           	    allocation.Add(budget);
+                remainingSpend -= budget;
+            }
+            else
+            {
+                //Person has enough to cover their allocation
+                allocation.Add(splitAllocation);
+                remainingSpend -= splitAllocation;
+            }
+
+        }
+
+        //Print budget allocations
+        allocation.ForEach(p=> Console.WriteLine(p)); 
         
     }
-}
 
-
-public static int[] SplitBudgetAllocation(int totalAmount, int splitCount)
-{
-    int[] splitAmount = new int[splitCount];
-
-    int remainder = totalAmount;
-
-    for (int i = 0; i < splitCount; i++)
+    public static int SplitBudgetAllocation(int totalAmount, int splitCount)
     {
-        int split = remainder / (splitCount - i);
-        Console.Error.WriteLine(split);
-
-        splitAmount[i] = split;
-        remainder -= split;
+        return totalAmount / splitCount;
     }
 
-    return splitAmount;
 }
