8cf1f6b7859663fcbe4a90c6c571c35ba959a403
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-03-07 17:30:01 +1300

    Create Seven Degrees of Kevin Bacon.cs
    Start

---
+++ C#/Easy/Seven Degrees of Kevin Bacon.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string actorName = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string movieCast = Console.ReadLine();
+        }
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("N degrees to Kevin Bacon");
+    }
+}
